d897ead425d542a231e08ee7ac37dfcee533d3dd
mogeritz <moge.world.kanal@gmail.com>
2022-10-27 16:13:43 -0700

    Finished first part of explore section

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,32 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const speechsynth = window.speechSynthesis;
+  const voiceoptions = speechsynth.getVoices();
+
+  const voiceselect = document.getElementById("voice-select");
+  const text = document.getElementById("text-to-speak");
+  const button = document.getElementsByTagName("button")[0];
+  const img = document.getElementsByTagName("img")[0];
+
+  setInterval(() => 
+  {
+    if(speechsynth.speaking) img.src = "assets/images/smiling-open.png";
+    else     img.src = "assets/images/smiling.png";
+  }, 100);
+
+  for(var i = 0; i < voiceoptions.length; i++){
+    var node = document.createElement('option');
+    node.value = voiceoptions[i].name;
+    node.innerHTML = voiceoptions[i].name;
+    voiceselect.appendChild(node);
+  }
+
+  button.addEventListener("click", (event)=>{
+    console.log(voiceselect.value);
+    let utterance = new SpeechSynthesisUtterance(text.value);
+    utterance.voice = voiceoptions.find((a,b,c)=>{a.name == voiceselect.value});
+    speechsynth.speak(utterance);
+  });
+
 }
